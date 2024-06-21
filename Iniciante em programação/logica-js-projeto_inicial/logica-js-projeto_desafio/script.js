590ceb452edc2df612d20befef595159397532a3
Alice <alicessr12@gmail.com>
2024-06-20 22:11:43 -0300

    Lógica de programação primeira parte

---
+++ Iniciante em programação/logica-js-projeto_inicial/logica-js-projeto_desafio/script.js
@@ -0,0 +1,38 @@
+//Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert('Boas vindas ao nosso site!');
+
+//Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = "lua";
+
+//Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
+
+//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroVendas = 50;
+
+//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
+
+console.log(saldoDisponivel);
+
+//Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert("Erro! Preencha todos os campos");
+
+//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+alert(mensagemDeErro);
+
+//Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+let name = prompt('Qual o seu nome?');
+
+//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+let age = prompt('Qual sua idade?');
+
+//Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+if(age>18){
+    alert('Pode tirar a habilitação!');
+}
+else{
+    alert('Não pode tirar a habilitação');
+}
