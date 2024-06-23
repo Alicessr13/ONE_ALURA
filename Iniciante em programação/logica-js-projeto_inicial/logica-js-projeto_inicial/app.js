4bf7059eb44ecedaadd97e3f7ba53f077dbcc9d9
Alice <alicessr12@gmail.com>
2024-06-22 23:25:56 -0300

    ajustes

--- Iniciante em programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Iniciante em programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,13 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 5;
+    let numeroSecreto = 5;
 
-console.log(numeroSecreto)
+    console.log(numeroSecreto)
 
-let chute = prompt('Escolha um número entre 1 e 10')
+    let chute;
 
-if(numeroSecreto == chute){
-    alert(`Isso ai! Você descobriu o número ${numeroSecreto}`)
-} else{
-    alert('Você errou :( ')
+
+while (chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número entre 1 e 10')
+
+    if (numeroSecreto == chute) {
+        alert(`Isso ai! Você descobriu o número ${numeroSecreto}`)
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`)
+        } else {
+            alert(`O número secreto é menor que ${chute}`)
+        }
+    }
 }
