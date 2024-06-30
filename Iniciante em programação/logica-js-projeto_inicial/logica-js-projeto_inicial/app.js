c5d16c4224273a5902cba387809817a1e02c8505
Alice <alicessr12@gmail.com>
2024-06-29 21:20:20 -0300

    ajustes e desafio

--- Iniciante em programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Iniciante em programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,20 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
 
-    let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 +1);
 
-    console.log(numeroSecreto)
+console.log(numeroSecreto)
 
-    let chute;
+let chute;
 
-    let tentativas = 1;
+let tentativas = 1;
 
 
 while (chute != numeroSecreto) {
 
-    chute = prompt('Escolha um número entre 1 e 10')
+    chute = prompt('Escolha um número entre 1 e 100')
 
     if (numeroSecreto == chute) {
-        alert(`Isso ai! Você descobriu o número ${numeroSecreto} com ${tentativas} tentativas`)
+        break; //Para o código
     } else {
         if (numeroSecreto > chute) {
             alert(`O número secreto é maior que ${chute}`)
@@ -24,3 +24,15 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+
+//tentativas e meior que 1? se sim primeira opção senão segunda opção depois do : 
+//operador ternario
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`)
+
+//if (tentativas == 1) {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`)
+//} else {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`)
+//}
